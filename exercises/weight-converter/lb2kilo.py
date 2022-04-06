30fbde7a7e9255e3bc6adf737a976f83240f1ff8
HamingnottT <Jnottingham836@yahoo.com>
2022-04-06 13:54:30 -0500

    python exercises update

---
+++ exercises/weight-converter/lb2kilo.py
@@ -0,0 +1,20 @@
+def main():
+    print("This program can convert your weight from lbs to kilos and vice versa.")
+    
+    weight = int(input("Input your weight here: "))
+
+    metric = input("(L)bs or (K)g: ").upper()
+
+    pounds2kilos = round(weight/2.20462, 2) # or weight*0.45
+    kilos2pounds = round(weight*2.20462, 2) # or weight/0.45
+
+    if metric == "L":
+        print(f"Your weight is {pounds2kilos} kg")
+    elif metric == "K":
+        print(f"Your weight is {kilos2pounds} lbs")
+    else:
+        print("Error: wrong input")
+
+
+if __name__ == '__main__':
+    main()
---
+++ exercises/weight-converter/main.py
@@ -0,0 +1,7 @@
+import lb2kilo
+
+def main():
+    lb2kilo.main()
+
+if __name__ == '__main__':
+    main()
